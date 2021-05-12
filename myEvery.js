faec2e02438e780e3a625e6fc7ccbe1b81b3c939
Hayk Bazoyan <bazoyan13@gmail.com>
2021-05-12 16:23:23 +0400

    fix: fix bug about empty array

--- myEvery.js
+++ myEvery.js
@@ -1,11 +1,12 @@
 function myEvery(array, fn) {
+  if (array.length === 0) return true;
   for (let index = 0; index < array.length; index++) {
     const element = array[index];
     return fn(element, index, array) ? true : false;
   }
 }
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let arr = [];
 let arr1 = [11, 12, 13, 14, 5, 6, 7, 8];
 let print = myEvery(arr, (el) => el > 4);
 console.log(print);
