51989bba891958b5288de8ba185ed899ccb21070
Hayk Bazoyan <bazoyan13@gmail.com>
2021-05-12 15:50:13 +0400

    feat: Two tasks codes

--- passArguments.js
+++ passArguments.js
@@ -1,5 +1,15 @@
+// How to pass arguments in sum function?
+
 function sum(x, y, z) {
   return x + y + z;
 }
 const numbers = [1, 2, 3];
 console.log(sum(...numbers)); // prints 6
+
+// Swapping Values using the Destructuring Assignment
+
+let a = 3;
+let b = 6;
+[a, b] = [b, a];
+console.log(a);
+console.log(b);
