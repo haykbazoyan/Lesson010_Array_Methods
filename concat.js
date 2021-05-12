7627b857d8fd15d1823da3ef550f5cd18f3d3256
Hayk Bazoyan <bazoyan13@gmail.com>
2021-05-12 15:26:04 +0400

    feat: add new code

--- concat.js
+++ concat.js
@@ -5,3 +5,11 @@ const arr2 = [5, 6, 7, 8, 9];
 const arr3 = arr1.concat(arr2);
 
 console.log(arr3);
+
+// with destructuring assignment
+
+const arr1_1 = [1, 2, 3, 4];
+const arr2_2 = [5, 6, 7, 8, 9];
+const [...arr3_3] = [...arr1_1, ...arr2_2];
+
+console.log(arr3_3);
